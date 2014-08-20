92cd0ba264c3468013118840a4abe705771a5f16
dperuo <derek@dbpventures.com>
2014-08-20 11:25:50 -0400

    data loads correctly

--- app/offers.js
+++ app/offers.js
@@ -2,9 +2,17 @@ $(function(){
 
   $.getJSON('app/offers.json', function(data) {
 
-    $(data.offers).each(function (index, offer) {
-      $('.offers__panel').append('<div class="offers__panel-item" data-cat="' + offer.category + '"><dt class="offers__panel-header">' + offer.header + '</dt><dd class="offers__panel-img"><img src="' + offer.image + '"></dd><dd class="offers__panel-price">' + offer.price + '</dd><a class="offers__panel-link" href="#">View Deal</a></div>')
+    var catArray = [],
+        template = $('#mustache').html(),
+        html     = Mustache.render(template, data);
+
+    data.offers.forEach(function (offer, index) {
+      catArray.push(offer.category)
     })
+
+    alert(catArray)
+
+    $('.offers__panel').html(html);
   })
 
   var catItems = $('.offers__cat-list-item');
@@ -16,5 +24,4 @@ $(function(){
     catItems.removeClass(activeClass);
     $(this).addClass(activeClass);
   }
-
 });
