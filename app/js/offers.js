d4af0ad677b5d3b108814553ef1f8fdad1a4ac04
dperuo <derek@dbpventures.com>
2014-08-21 18:40:53 -0400

    housecleaning

--- app/js/offers.js
+++ app/js/offers.js
@@ -1,7 +1,6 @@
 $(function(){
 
   var activeClass = 'offers__cat-list-item--active',
-      catArray    = [],
       catItems    = '.offers__cat-list-item',
       json        = 'app/js/offers.json',
       offerPanel  = '.offers__panel';
@@ -11,11 +10,6 @@ $(function(){
 
   // --------------- GET JSON --------------- //
   $.getJSON(json, function(data) {
-
-    data.offers.forEach(function (offer) {
-      catArray.push(offer.category);
-    });
-
     $(offerPanel).html(Mustache.render(template, data));
   });
 
